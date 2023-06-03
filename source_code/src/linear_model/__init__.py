84f7f2b6b6b374f5fb31034bd1da58b86cdf4190
stefanus-yudi-irwan <stefanus.yudi96@gmail.com>
2023-06-03 12:33:05 +0700

    feat : begin code linear regression

---
+++ source_code/src/linear_model/__init__.py
@@ -0,0 +1,5 @@
+from .base import LinearRegression
+
+__all__  = [
+    "LinearRegression"
+]
--- source_code/src/linear_model/base.py
+++ source_code/src/linear_model/base.py
@@ -1,3 +1,5 @@
+"""base class for linear model"""
+
 import numpy as np
 
 class LinearRegression:
@@ -20,5 +22,19 @@ class LinearRegression:
             # Create A
             A = X
         
-        # Solve for model parameters, theta
+        # Find model parameter using closed form solution
         theta = np.linalg.inv(A.T @ A) @ A.T @ y 
+
+        # Extract model parameters
+        if self.fit_intercept:
+            self.coef_ = theta[:n_features]
+            self.intercept_ = theta[-1]
+        else:
+            self.coef_ = theta
+            self.intercept_ = 0.0
+
+    def predict(self, X):
+        X = np.array(X)
+        y_pred = np.dot(X,self.coef_) + self.intercept_
+
+        return y_pred
